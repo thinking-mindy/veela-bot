1f00142d2989355b61707c5c406617f715d69e31
Talent Chikamhi <67649495+thinking-mindy@users.noreply.github.com>
2024-09-11 12:01:43 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,64 @@
+## Hi, i'm Veela😉
+
+- [Reply Now](https://thinkingminds.co.zw/to/bot)
+
+
+## Install
+- `npm i @thinkingminds/veela`
+
+## Overview
+- This is a chatbot based module which can help you create chatbots in web, mobile apps. The chatbot comes with build in responses which are limited and therefore one can add more responses as per requirement. This the free version thus its limited of responses and intelligence but the [Premium] version is comming soon which will be very intelligent and will only be costing from [$69].
+
+- For students who wish to create thier own chatbots, should it be for school assignment or self app, feel free to use this project's free version as a starting point.
+
+## History
+This program was firstly developed in Node.js but later on switched to Rust. Even though it's core is written in Rust, there's still a Node.js API available for  
+## For developers
+If you need, by any chance to modify the source code, you so allowed to do so. Also feel free to resuse and redeploy. If you also have any suggestion or a better insight please feel free to contact me on the contact details provided below.
+
+To use this in your node js app, simply, import the [DA] function and simply pass the text you want to be evaluated. The response can be stored in any variable of your choice.
+
+## Example usage
+
+> Node js
+```javascript
+const da=require('@thinkingminds/veela');
+const readline=require('readline').createInterface({input: process.stdin,output: process.stdout});
+
+readline.question('Reply: ',req=>{
+    const response=da.DA(req);
+	console.log(response)
+	readline.close();
+	});
+```
+> React
+```javascript
+import {DA} from '@thinkingminds/veela'
+export default function App(){
+
+	const [res,setRes]=useState();
+	
+	useEffect(()=>setRes(DA("your_text here"),[])
+	return(
+	<div>{res}</div>
+	)
+}
+```
+
+## Reporting Issues
+If you'd like to report a bug then: contact me as follows:
+
+- Call/ WhatsApp: +263771657582
+- Email: talentjahtale@gmail.com
+
+## Licensing
+
+- Copyright 2024 Thinking Minds LLC (https://thinkingminds.co.zw)
+
+- Licensed under Thinking Minds LLC
+
+## Useful Links
+
+- [Thinking Minds](https://thinkingminds.co.zw)
+
+##  THINKING MINDS
--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,84 @@
-
 use serde_json::json;
 
-fn veela(data:&String)->String{
-    let vdata=match std::fs::read_to_string("./src/data/mind"){Ok(file)=>{file},Err(_)=>"None".to_string()};
-    let vdata=match serde_json::from_str(&vdata) {Ok(drjson)=>{drjson},Err(_)=>{serde_json::json!({"sym":"None","dis":"None"})}};
-    
+fn veela(data: &String) -> String {
+    let vdata = match std::fs::read_to_string("./src/data/mind") {
+        Ok(file) => file,
+        Err(_) => "None".to_string(),
+    };
+    let vdata = match serde_json::from_str(&vdata) {
+        Ok(drjson) => drjson,
+        Err(_) => {
+            serde_json::json!({"sym":"None","dis":"None"})
+        }
+    };
+
     //clean my data, remove stop words and charcters
     let junk = regex::Regex::new(r"\n|\d|\W").unwrap();
     let junk_words = regex::Regex::new(vdata["stop"].as_str().unwrap()).unwrap();
-    let user_input=junk.replace_all(data.to_lowercase().as_str(), " ").to_string();
-    let user_input=junk_words.replace_all(user_input.as_str(), "").to_string();
+    let user_input = junk
+        .replace_all(data.to_lowercase().as_str(), " ")
+        .to_string();
+    let user_input = junk_words.replace_all(user_input.as_str(), "").to_string();
 
     //remove empty strings
-    let mut tokens:Vec<&str>=user_input.split(' ').filter(|x|x!=&"").collect();
+    let mut tokens: Vec<&str> = user_input.split(' ').filter(|x| x != &"").collect();
 
     //remove duplicates tokens
     tokens.sort();
     tokens.dedup();
 
-    let mut results:Vec<serde_json::Value>=Vec::new();
-    let mut final_result:String=String::new();
+    let mut results: Vec<serde_json::Value> = Vec::new();
+    let mut final_result: String = String::new();
 
-    if tokens.len()<2 {
-        for d in vdata["oneword"].as_array().unwrap(){
-            let mut rate=0;
-            for data in &tokens{
-                let v=d["keys1"].as_str().unwrap().to_string().to_lowercase();
-                if v.find(data.to_lowercase().as_str())!=None{rate=rate+1}
+    if tokens.len() < 2 {
+        for d in vdata["oneword"].as_array().unwrap() {
+            let mut rate = 0;
+            for data in &tokens {
+                let v = d["keys1"].as_str().unwrap().to_string().to_lowercase();
+                if v.find(data.to_lowercase().as_str()) != None {
+                    rate = rate + 1
+                }
             }
             results.push(json!({"rp":d["rp"],"rate":rate,"sug":d["sug"]}))
         }
-        let mut results:Vec<serde_json::Value>=results.into_iter().filter(|x|x["rate"].as_i64().unwrap()>0).collect();
-        results.sort_by_key(|f|f["rate"].as_i64());
+        let mut results: Vec<serde_json::Value> = results
+            .into_iter()
+            .filter(|x| x["rate"].as_i64().unwrap() > 0)
+            .collect();
+        results.sort_by_key(|f| f["rate"].as_i64());
 
-        final_result=match results.last(){
-            Some(value)=>{value["rp"][0].to_string()},
-            None=>{
-                vdata["fallback"]["rp"][0].to_string()
-            }};
-    }else{
-        for d in vdata["morewords"].as_array().unwrap(){
-            let mut rate=0;
-            for data in &tokens{
-                let v=d["keys1"].as_str().unwrap().to_string().to_lowercase();
-                if v.find(data.to_lowercase().as_str())!=None{rate=rate+1}
+        final_result = match results.last() {
+            Some(value) => value["rp"][0].to_string(),
+            None => vdata["fallback"]["rp"][0].to_string(),
+        };
+    } else {
+        for d in vdata["morewords"].as_array().unwrap() {
+            let mut rate = 0;
+            for data in &tokens {
+                let v = d["keys1"].as_str().unwrap().to_string().to_lowercase();
+                if v.find(data.to_lowercase().as_str()) != None {
+                    rate = rate + 1
+                }
             }
             results.push(json!({"rp":d["rp"],"rate":rate,"sug":d["sug"]}))
         }
-        let mut results:Vec<serde_json::Value>=results.into_iter().filter(|x|x["rate"].as_i64().unwrap()>0).collect();
-        results.sort_by_key(|f|f["rate"].as_i64());
-    
-        final_result=match results.last(){
-            Some(value)=>{value["rp"][0].to_string()},
-            None=>{
-                vdata["fallback"]["rp"][0].to_string()
-            }};
-        }
+        let mut results: Vec<serde_json::Value> = results
+            .into_iter()
+            .filter(|x| x["rate"].as_i64().unwrap() > 0)
+            .collect();
+        results.sort_by_key(|f| f["rate"].as_i64());
+
+        final_result = match results.last() {
+            Some(value) => value["rp"][0].to_string(),
+            None => vdata["fallback"]["rp"][0].to_string(),
+        };
+    }
 
     final_result
-
 }
 
 fn main() {
-    let umsg: String="Hello".to_string();
-    let res=veela(&umsg);
-    println!("The response is {:?}",res);
-
+    let umsg: String = "you are lame".to_string();
+    let res = veela(&umsg);
+    println!("The response is {:?}", res);
 }
-
