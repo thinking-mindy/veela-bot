111d0b35ba063257ac8adece95815d11f4db3397
Talent Chikamhi <67649495+thinking-mindy@users.noreply.github.com>
2025-01-19 11:40:34 +0200

    Add files via upload

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use std::env::args;
+use serde_json::json;
 
 #[tokio::main]
 async fn main() {
-    let vee=veela::mind::veela_online(args().nth(1).unwrap().to_string()).await;
+    let user_input="help".to_string();
+    let r_m=veela::mind::roam_memories(user_input.clone(),veela::mind::get_memories());
+    let vee=veela::mind::veela_local(user_input.clone());
     let begin_veela=veela::mind::make_sentance(veela::mind::learn(vee));
-    println!("{}",begin_veela);
+    if r_m.is_empty() {veela::mind::keep_memory(json!({"q":user_input,"rp":begin_veela}));}
+    let f_result=if !r_m.is_empty() {r_m}else{begin_veela};
+    println!("{}",f_result);
 }
--- src/mind.rs
+++ src/mind.rs
@@ -1,7 +1,6 @@
+use std::io::Write;
 use rand::{self, Rng};
 use serde_json::{json, Value};
-use search_with_google::Client;
-
 
 pub fn get_learning_data() -> String {
     match std::fs::read_to_string("data/ll.txt") {
@@ -17,19 +16,16 @@ pub fn get_learned_data() -> String {
     }
 }
 
+pub fn get_memories() -> String {
+    match std::fs::read_to_string("data/memories") {
+        Ok(file) => file,
+        Err(_) => "None".to_string(),
+    }
+}
+
 pub fn make_sentance(data: Vec<Value>) -> String {
     let mut s: Vec<String> = vec![];
-
     let learned: Vec<Value> = data;
-
-    /*convert back to json values
-    for d in data.split("\n").into_iter() {
-        if d != "" {
-            let tm: Value = serde_json::from_str(&d).unwrap();
-            learned.push(tm)
-        }
-    }
-    */
 
     //group start words
     let mut rn = rand::thread_rng();
@@ -91,6 +87,7 @@ pub fn make_sentance(data: Vec<Value>) -> String {
                 let tnxt: Vec<&str> = str.split("0x5").collect();
                 let ttnxt = tnxt[rnv.gen_range(0..tnxt.len())];
                 res.push(ttnxt.to_string());
+                println!("{:?}",res);
 
                 for b in bb.clone() {
                     let bref = b.get("w").unwrap().to_string();
@@ -114,6 +111,45 @@ pub fn make_sentance(data: Vec<Value>) -> String {
     }
 
     add_body(sobject, s, body).join(" ")
+}
+
+pub fn roam_memories(d:String,mem:String)->String{
+    let mut memories:Vec<Value>=vec![];
+    let mut result:Vec<String>=vec![];
+    let data:Vec<String>=d.split(" ").map(|x|x.to_string()).collect();
+    for d in mem.split("\n").into_iter() {
+        if d != "" {
+            let tm: Value = serde_json::from_str(&d).unwrap();
+            memories.push(tm)
+        }
+    }
+
+    if memories.len()==0{
+        "".to_string()
+    }else{
+        for d in memories {
+            let tokens:Vec<String>=d["q"].to_string().split(" ").map(|x|x.to_string()).collect();
+            let mut rate = 0;
+
+            for t in tokens {
+                for dt in &data{
+                    if dt.to_lowercase() == t.to_lowercase(){
+                        rate = rate + 1
+                    }
+                }
+            }
+
+            if (rate/data.len()).to_string()<0.8.to_string(){
+                result.push(d["rp"].to_string())
+            }
+        }
+
+        if !result.is_empty(){
+            result[0].to_string()
+        }else{
+            "".to_string()
+        }
+    }
 }
 
 pub fn veela_local(data: String) -> String {
@@ -196,9 +232,9 @@ pub fn veela_local(data: String) -> String {
 
 pub async fn veela_online(data: String)->String{
     let result:String;
-    let client = Client::default();
+    let client = search_with_google::Client::default();
     let results = client.search(&data, 3, None).await;
-    println!("{:?}",results);
+    //println!("{:?}",results);
     if let Ok(result_list) = results {
         result=result_list[0].description.to_string();
     }else{
@@ -283,4 +319,13 @@ pub fn learn(data: String)->Vec<Value> {
     }
        */
       fresult
+}
+
+pub fn keep_memory(data:Value){
+    //save learned data
+    let mut file = std::fs::OpenOptions::new()
+        .append(true)
+        .open("data/memories")
+        .unwrap();
+        file.write((serde_json::to_string(&data).unwrap() + "\n").as_bytes()).unwrap();
 }
